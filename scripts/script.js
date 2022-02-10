c5562e6ab3d6da5a91dd4ad5ae10789b9ae392b5
Karim <abdelkarim.kh95@gmail.com>
2022-02-10 03:15:58 +0200

    removing console  password.value

--- scripts/script.js
+++ scripts/script.js
@@ -113,7 +113,6 @@ async function fetchPasswordApi() {
         const data = await response.json();
         console.log(data);
         //in passwordAPI.php if password input length < 8 viable password = 1
-        console.log(passwordInput.value);
         if (data.viablePassword == false && passwordInput.value.length < 8){
             alert("password should contain at least 8 characters");
         }else if(data.viablePassword == true){
